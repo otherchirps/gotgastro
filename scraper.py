83a862ef1e4d856215a5736199f47fa3cb4a0689
MarkMad <MarkMad@users.noreply.github.com>
2015-06-25 21:00:55 +1000

    Update scraper.py
    First try

--- scraper.py
+++ scraper.py
@@ -1,24 +1,20 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://www.foodauthority.nsw.gov.au/penalty-notices/default.aspx?template=results&searchname=Added%20this%20month&inthelast=31")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+root = lxml.html.fromstring(html)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+for tr in root.cssselect("#myTable tr"):
+	td = tr.find("td")
+	record={}
+	record["penaltynotice"]=td[3]
+	record["tradename"]=td[0]
+	record["suburb"]=td[1]
+	scraperwiki.sqlite.save(unique_keys=['penaltynotice'], data=record)
+
